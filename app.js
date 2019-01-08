561ec1ec3780e4d430559ab4ec1459164ed67981
zhangye <17628038367@163.com>
2019-01-08 10:04:16 +0800

    update store goods router

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var storeRouter = require('./routes/store');
+var goodsRouter = require('./routes/goods');
 
 
 var app = express();
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/store', storeRouter);
+app.use('/goods', goodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
