da7e5e2bb3aec91278503e0865d7a46abb01b899
yzx <283232992@qq.com>
2019-01-08 10:46:58 +0800

    sppiler供应商详情

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var storeRouter = require('./routes/store');
-
+var suppilerRouter = require('./routes/suppiler');
 
 var app = express();
 
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/store', storeRouter);
-
+app.use('/suppiler', suppilerRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
---
+++ routes/suppiler.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+const db = require("ykt-mongo");
+const multiparty = require('multiparty');
+const path = require('path');
+const client = require('ykt-http-client');
+client.url('127.0.0.1:8080');
+//增加
+router.post('/', async function (req, res) {
+  let { supp_number, supp_name, supp_add, supp_phone, supp_web, supp_bus_pic, supp_note} = req.body;
+  client.post('/suppiler', { supp_number, supp_name, supp_add, supp_phone, supp_web, supp_bus_pic, supp_note  })
+  res.send({ status: 1 });
+});
+//查找
+router.get('/', async function (req, res) {
+  let data = await client.get("/suppiler", { page, rows, submitType: "findJoin", ref: "suppiler"});
+  res.send(data)
+  console.log(data)
+});
+//删除
+router.delete('/:id', async function (req, res) {
+  let id = req.params.id;
+  await client.delete("/suppiler/" + id);
+  res.send({ status: 1 });
+});
+
+//修改
+router.put("/:id", async function (req, res) {
+  let id = req.params.id;
+  let { supp_number, supp_name, supp_add, supp_phone, supp_web, supp_bus_pic, supp_note} = req.body
+  await client.put("/suppiler/" + id, {
+    supp_number, supp_name, supp_add, supp_phone, supp_web, supp_bus_pic, supp_note  
+  });
+  res.send({ status: 1 });
+
+});
+//根据id进行查找
+router.get('/:id', async function (req, res) {
+  let id = req.params.id;
+  let data = await client.get("/suppiler/" + id);
+  res.send(data);
+})
+//图片
+router.post("/upload", function (req, res) {
+  let form = new multiparty.Form({ 
+      uploadDir: "./public/upload"
+  });
+  form.parse(req, function (err, fields, suppiler) {
+      if (err) {
+          res.send(err);
+      } else {
+          res.send(path.basename(suppiler[Object.keys(files)[0]][0].path));
+      }
+  });
+});
+
+module.exports = router;
