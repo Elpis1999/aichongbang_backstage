e750bb0d7679bdd414478672db241c9db60e1725
zhangye <17628038367@163.com>
2019-01-09 14:02:34 +0800

    update

--- routes/suppilergoods.js
+++ routes/suppilergoods.js
@@ -9,20 +9,39 @@ client.url('127.0.0.1:8080');
 
 // 查询
 router.get('/', async function (req, res) {
-    let { page, rows, type, value, supId } = req.query;
-    let datas = await client.get("/suppilergoods", { page, rows, submitType: "findJoin", ref: "suppiler" });
-    // console.log(data)
+    let {
+        page,
+        rows,
+        type,
+        value,
+        supId
+    } = req.query;
+    let searchObj = {};
+    if (type) {
+        searchObj = {
+            [type]: value || ""
+        };
+    }
+    let datas = await client.get("/suppilergoods", {
+        page,
+        rows,
+        ...searchObj,
+        submitType: "findJoin",
+        ref: "suppiler"
+    });
     let data = [];
+    let newData = datas;
+    datas = datas.rows;
     if (supId) {
         for (let i = 0; i < datas.length; i++) {
-            if (supId == datas[i].suppiler.$id) {
+            if (supId == datas[i].suppiler._id) {
                 data.push(datas[i])
             }
         }
-        res.send(data);
+        newData.rows = data;
+        res.send(newData);
     } else {
-        data = datas;
-        res.send(data);
+        res.send(newData);
     }
 })
 //通过ID查询
@@ -43,19 +62,53 @@ router.delete('/:id', async function (req, res) {
 
 //添加
 router.post('/', async function (req, res) {
-    let { supp_gd_brand, supp_gd_title, supp_gd_type, supp_gd_material,
-        made, supp_gd_appl, supp_gd_exc, supp_gd_install, supp_gd_taste,
-        supp_gd_special, supp_gd_from, supp_gd_factor, supp_gd_keepquality,
-        supId, supp_gd_specialinfo, supp_gd_price, bigpic, smallpic } = req.body;
+    let {
+        supp_gd_brand,
+        supp_gd_title,
+        supp_gd_type,
+        supp_gd_material,
+        made,
+        supp_gd_appl,
+        supp_gd_exc,
+        supp_gd_install,
+        supp_gd_taste,
+        supp_gd_special,
+        supp_gd_from,
+        supp_gd_factor,
+        supp_gd_keepquality,
+        supId,
+        supp_gd_specialinfo,
+        supp_gd_price,
+        bigpic,
+        smallpic
+    } = req.body;
 
     await client.post('/suppilergoods', {
-        supp_gd_brand, supp_gd_title, supp_gd_type, supp_gd_material,
-        made, supp_gd_appl, supp_gd_exc, supp_gd_install, supp_gd_taste,
-        supp_gd_special, supp_gd_from, supp_gd_factor, supp_gd_keepquality,
-        supp_gd_suppiler: { $ref: "suppiler", $id: supId, },
-        supp_gd_specialinfo, supp_gd_price, bigpic, smallpic
+        supp_gd_brand,
+        supp_gd_title,
+        supp_gd_type,
+        supp_gd_material,
+        made,
+        supp_gd_appl,
+        supp_gd_exc,
+        supp_gd_install,
+        supp_gd_taste,
+        supp_gd_special,
+        supp_gd_from,
+        supp_gd_factor,
+        supp_gd_keepquality,
+        supp_gd_suppiler: {
+            $ref: "suppiler",
+            $id: supId,
+        },
+        supp_gd_specialinfo,
+        supp_gd_price,
+        bigpic,
+        smallpic
     })
-    res.send({ status: 1 });
+    res.send({
+        status: 1
+    });
 });
 
 //图片
